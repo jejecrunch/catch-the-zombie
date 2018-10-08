00af000093bb7d8888d0dda7000369437dfca17d
jang-eunbin <jang-eunbin@192.168.0.10>
2018-10-08 17:58:32 +0900

    음악삽입

--- src/home/CatchTheZombie.java
+++ src/home/CatchTheZombie.java
@@ -1,13 +1,14 @@
 package home;
 
-//import java.awt.Color;
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
-//import java.awt.event.MouseAdapter;
-//import java.awt.event.MouseEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseEvent;
 
 import javax.swing.ImageIcon;
-//import javax.swing.JButton;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class CatchTheZombie extends JFrame {
@@ -15,17 +16,18 @@ public class CatchTheZombie extends JFrame {
 	private Image screenImage;
 	private Graphics screenGraphic;
 
-	
-	//private ImageIcon startButtonBasicImage = new ImageIcon(Main.class.getResource("../images/startButtonBasic.png"));
-	//private ImageIcon startButtonEnteredImage = new ImageIcon(Main.class.getResource("../images/startButtonEntered.png"));
-	//private ImageIcon quitButtonBasicImage = new ImageIcon(Main.class.getResource("../images/quitButtonBasic.png"));
-	//private ImageIcon quitButtonEnteredImage = new ImageIcon(Main.class.getResource("../images/quitButtonEntered.png"));
 
-	//private JButton startButton = new JButton(startButtonBasicImage);
-	//private JButton quitButton = new JButton(quitButtonBasicImage);
-	private Image introBackground = new ImageIcon(Main.class.getResource("../images/introBackground.jpg")).getImage();
+	private ImageIcon startButtonBasicImage = new ImageIcon(Main.class.getResource("../images/startButtonBasic.png"));
+	private ImageIcon startButtonEnteredImage = new ImageIcon(Main.class.getResource("../images/startButtonEntered.png"));
+	private ImageIcon quitButtonBasicImage = new ImageIcon(Main.class.getResource("../images/quitButtonBasic.png"));
+	private ImageIcon quitButtonEnteredImage = new ImageIcon(Main.class.getResource("../images/quitButtonEntered.png"));
 
-	//private int mouseX, mouseY;
+	private JButton startButton = new JButton(startButtonBasicImage);
+	private JButton quitButton = new JButton(quitButtonBasicImage);
+
+	private Image background = new ImageIcon(Main.class.getResource("../images/introBackground.jpg")).getImage();
+
+	private int mouseX, mouseY;
 
 	public CatchTheZombie() { 
 		setTitle("온수행");
@@ -35,14 +37,9 @@ public class CatchTheZombie extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 게임창을 닫았을 떄 완전히 종료 되도록 함
 		setVisible(true);	
 		
-		Music introMusic = new Music("introMusic.mp3", true);
-		introMusic.start();
-		
 		//setBackground(new Color(0, 0, 0, 0)); // paintCompnent 했을 때 배경을 하얀색으로 해줌
-		//setLayout(null); // button이나 JRabel을 넣었을 때 그 위치 그대로 넣게 함
+		setLayout(null); // button이나 JRabel을 넣었을 때 그 위치 그대로 넣게 함
 
-		
-		/*
 		startButton.setBounds(40, 200, 100, 30); // x, y, w, h
 		startButton.setBorderPainted(false);
 		startButton.setContentAreaFilled(false);
@@ -51,18 +48,28 @@ public class CatchTheZombie extends JFrame {
 			@Override
 			public void mouseEntered(MouseEvent e) {
 				startButton.setIcon(startButtonEnteredImage);
+				startButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+				Music buttonEnteredMusic = new Music("buttonEnteredMusic.mp3", false);
+				buttonEnteredMusic.start();
 			}
 			@Override
 			public void mouseExited(MouseEvent e) {
 				startButton.setIcon(startButtonBasicImage);
+				startButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
 			}
 			@Override
 			public void mousePressed(MouseEvent e) {
-				// 게임 시작 이벤트 
+				Music buttonEnteredMusic = new Music("buttonPressedMusic.mp3", false);
+				buttonEnteredMusic.start();
+				startButton.setVisible(false);
+				quitButton.setVisible(false);
+				//
+				background = new ImageIcon(Main.class.getResource("../images/imtroBackground.jpg"))
+						.getImage();
 			}
 		});
 		add(startButton);
-		
+
 		quitButton.setBounds(40, 330, 100, 30);
 		quitButton.setBorderPainted(false);
 		quitButton.setContentAreaFilled(false);
@@ -71,13 +78,19 @@ public class CatchTheZombie extends JFrame {
 			@Override
 			public void mouseEntered(MouseEvent e) {
 				quitButton.setIcon(quitButtonEnteredImage);
+				quitButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+				Music buttonEnteredMusic = new Music("buttonEnteredMusic.mp3", false);
+				buttonEnteredMusic.start();
 			}
 			@Override
 			public void mouseExited(MouseEvent e) {
 				quitButton.setIcon(quitButtonBasicImage);
+				quitButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
 			}
 			@Override
 			public void mousePressed(MouseEvent e) {
+				Music buttonEnteredMusic = new Music("buttonPressedMusic.mp3", false);
+				buttonEnteredMusic.start();
 				try {
 					Thread.sleep(1000);
 				} catch (InterruptedException ex) {
@@ -85,9 +98,11 @@ public class CatchTheZombie extends JFrame {
 				}
 				System.exit(0);
 			}
-		});
+		}); 
 		add(quitButton);
-		*/
+
+		Music introMusic = new Music("introMusic.mp3", true);
+		introMusic.start();
 	}
 
 	public void paint(Graphics g) {
@@ -98,7 +113,7 @@ public class CatchTheZombie extends JFrame {
 	}
 
 	public void screenDraw(Graphics g) {
-		g.drawImage(introBackground, 0, 0, null);
+		g.drawImage(background, 0, 0, null);
 		//paintComponents(g); // 고정된 정적인 이미지를 component 해줌
 		this.repaint(); // 전체 화면 이미지를 프로그램 종료될 때까지 매 순간마다 계속 반복하면서 그려줌
 	}
